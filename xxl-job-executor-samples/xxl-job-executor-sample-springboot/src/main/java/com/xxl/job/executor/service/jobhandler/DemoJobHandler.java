95a28cc9bc34f631e12d312ca2198583c287b9c4
yuruyi <ryyu@glprop.com>
2020-03-30 14:16:52 +0800

    修改 端口 等配置信息

--- xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/service/jobhandler/DemoJobHandler.java
+++ xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/service/jobhandler/DemoJobHandler.java
@@ -4,6 +4,8 @@ import com.xxl.job.core.biz.model.ReturnT;
 import com.xxl.job.core.handler.IJobHandler;
 import com.xxl.job.core.handler.annotation.JobHandler;
 import com.xxl.job.core.log.XxlJobLogger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.TimeUnit;
@@ -22,12 +24,15 @@ import java.util.concurrent.TimeUnit;
  */
 @JobHandler(value="demoJobHandler")
 @Component
+
 public class DemoJobHandler extends IJobHandler {
 
+
+	private Logger  Log = LoggerFactory.getLogger(DemoJobHandler.class);
 	@Override
 	public ReturnT<String> execute(String param) throws Exception {
 		XxlJobLogger.log("XXL-JOB, Hello World.");
-
+		Log.info("3333333"+ param);
 		for (int i = 0; i < 5; i++) {
 			XxlJobLogger.log("beat at:" + i);
 			TimeUnit.SECONDS.sleep(2);
